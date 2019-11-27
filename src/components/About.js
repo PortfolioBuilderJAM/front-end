4e9e9dc54e8f8e153f97f020615de9a032ecc226
Jasmine Kasheboon Khoury <jasminekh96@gmail.com>
2019-11-27 00:54:31 -0500

    Component setups

---
+++ src/components/About.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const About = () => {
+	return (
+		<div>
+			<h1>About</h1>
+			<p>Welcome to Portfolio Builder! Created by 4 students from Lambda...</p>
+		</div>
+	);
+};
+
+export default About;
